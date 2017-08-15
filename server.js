1b092e1ea93cb66db347e4996b724a3886cb6760
dvkcool <divyanshukumarg@gmail.com>
2017-08-15 20:51:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,6 +78,9 @@ res.send(createtemp(articles[articleName]));
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
