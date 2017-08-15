dc3f2a0c6535441e57bad65f726371e096622757
dvkcool <divyanshukumarg@gmail.com>
2017-08-16 00:01:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,6 +71,11 @@ function createtemp(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter=0;
+app.get('/counter', function(req, res){
+   counter++;
+   res.send(''+counter);
+});
 app.get('/:articleName', function (req, res) {
  var articleName= req.params.articleName;
 res.send(createtemp(articles[articleName]));
