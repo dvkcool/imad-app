8b53948ba59df6babbab51961b54d0da870df154
dvkcool <divyanshukumarg@gmail.com>
2017-08-16 00:02:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/', function (req, res) {
 var counter=0;
 app.get('/counter', function(req, res){
    counter++;
-   res.send(''+counter);
+   res.send(counter.toString());
 });
 app.get('/:articleName', function (req, res) {
  var articleName= req.params.articleName;
