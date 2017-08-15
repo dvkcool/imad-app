0567388f95cadc51262d202f9f917d9929d5e86c
dvkcool <divyanshukumarg@gmail.com>
2017-08-15 22:12:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var e= document.getElementById('main-text');
 e.innerHTML='New value';
 //moving img
 var img = document.getElementById('madi');
-img.onclick = function(){
+img.onclick = function () {
     img.style.marginleft='50000px';
    
 };
