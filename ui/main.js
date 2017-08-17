a1a0d0e328f3146b7398c373ecb2efb21427a0d8
dvkcool <divyanshukumarg@gmail.com>
2017-08-17 20:41:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ img.onclick = function () {
 var interval= setInterval(moveRight, 50);
 };
 var button = document.getElementById('b1');
-b1.onclick= function(){
+button.onclick= function(){
     var request= new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
@@ -36,17 +36,27 @@ b1.onclick= function(){
     request.send(null);
   
 };
-var nameInput = document.getElementById('name');
-var name1 = nameInput.value;
 var submit = document.getElementById('submit');
 submit.onclick= function(){
-  var names =['name1', 'name2', 'name3', 'name4'];
-  var list='';
-  for(var i=0; i<names.length; i++){
-      list=list+'<li>'+names[i]+'</li>';
-  }
-  var ul = document.getElementById('l1');
-  ul.innerHTML=list;
+   var request= new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status==200){
+              var names= request.responseText;
+              names=JSON.parse(names);
+              var list='';
+              for(var i=0; i<names.length; i++){
+                  list=list+'<li>'+names[i]+'</li>';
+              }
+               var ul = document.getElementById('l1');
+               ul.innerHTML=list;
+            }
+        }
+    };
+    var nameInput = document.getElementById('name');
+    var name1 = nameInput.value;
+    request.open('GET', '/submit-name?name='+name1, true );
+    request.send(null);
 };
 
 
