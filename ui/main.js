10b1d47e810c723d76019dd468a7be860f542d57
dvkcool <divyanshukumarg@gmail.com>
2017-08-17 20:19:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,3 +36,25 @@ b1.onclick= function(){
     request.send(null);
   
 };
+var nameInput = document.getElementById('name');
+var name1 = nameInput.value;
+var submit = document.getElementById('submit');
+submit.onclick= function(){
+  var names =['name1', 'name2', 'name3', 'name4'];
+  var list='';
+  for(var i=0; i<names.length; i++){
+      list=list+'<li>'+names[i]+'</li>';
+  }
+  var ul = document.getElementById('l1');
+  ul.innerHTML=list;
+};
+
+
+
+
+
+
+
+
+
+
